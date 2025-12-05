69e402c6c73bea9b99fdd987619760bbabdda1fb
Baibhav <baibhavtiwari37@gmail.com>
2025-12-05 21:22:08 +0530

    first commit

---
+++ Day-1.py
@@ -0,0 +1,29 @@
+a = 10
+b = 3.5
+c = True
+d = "Baibhav"
+
+print (type(a), type(b), type(c), type(d))
+
+
+
+buy_price = 100
+sell_price = 120
+
+quantity = 50
+
+profit = (sell_price - buy_price)
+profit_pct = (sell_price- buy_price)/ buy_price * 100
+
+print ("Profit:", profit)
+print ("Profit %:", profit_pct)
+
+
+
+x = 12
+y = 16
+
+print(x//y)
+print(x/y)
+print(x%y)
+print (2**5)
