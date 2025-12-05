59f1319efedfe7f08b5fe9113f3d37319431ac81
Baibhav <baibhavtiwari37@gmail.com>
2025-12-05 21:39:53 +0530

    Day-1

---
+++ Day-1/task.py
@@ -0,0 +1,24 @@
+buy_price = 250.5
+sell_price = 261.2
+quantity = 80
+
+profit = (sell_price - buy_price)
+profit_pct = (sell_price - buy_price)/ buy_price * 100
+
+print("profit :", profit)
+print("profit %:", profit_pct)
+
+
+trade_1 = 120
+trade_2 = -50
+trade_3 = 30
+trade_4 = 10
+trade_5 = -20
+trade_6 = 70
+trade_7 = -10
+
+total = (trade_1 + trade_2 + trade_3 + trade_4 + trade_5 + trade_6 + trade_7) - 17.5
+print("total: ", total)
+
+AvgPL = (trade_1 + trade_2 + trade_3 + trade_4 + trade_5 + trade_6 + trade_7)/ 7
+print("Average P&L:", AvgPL)
