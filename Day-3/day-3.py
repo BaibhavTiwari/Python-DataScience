e20b0ad9c0d3114a0cb30a782ec82428baf2088d
Baibhav <baibhavtiwari37@gmail.com>
2025-12-07 21:39:27 +0530

    day-3

---
+++ Day-3/day-3.py
@@ -0,0 +1,23 @@
+# Definition: a for loop goes through each element of a sequence
+
+pnl = [120, -50, 30, 10, -20, 70, -10]
+for x in pnl:
+  print(x)
+
+# Conditions let you react differently depending on data.
+
+# if conditions
+
+x=5;
+if x>0:
+  print("positive")
+
+# if-elif-else
+
+pnl=30
+if pnl > 0:
+  print("Positive")
+elif pnl < 0:
+  print("Negative")
+else:
+  print("Break-even")
