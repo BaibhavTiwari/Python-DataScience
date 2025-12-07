bcb0ce4a7cef6b49f5af2c5dd97b74c6e1bb6175
Baibhav <baibhavtiwari37@gmail.com>
2025-12-07 22:32:44 +0530

    task

--- Day-3/task.py
+++ Day-3/task.py
@@ -25,3 +25,23 @@ for x in pnl:
 
 print("Total profit trades:", profit_count)
 print("Total loss trades:  ", loss_count)
+
+#For each pnl in your P&L list, print “Big profit”, “Small profit”, “Small loss”, “Big loss” based on thresholds you choose (e.g. >50, between 0 and 50, etc.).
+
+pnl = [120, -50, 30, 10, -20, 70, -10]
+
+big_loss_count = 0
+
+for x in pnl:
+    if x > 50:
+        print(x, "→ Big profit")
+    elif x > 0:
+        print(x, "→ Small profit")
+    elif x >= -50:
+        print(x, "→ Small loss")
+    else:
+        print(x, "→ Big loss")
+        big_loss_count += 1
+
+print("Total Big Loss trades:", big_loss_count)
+
